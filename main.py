20faef485c625a565efbba038277656939ea28a4
tefy <tefy.rakotomahefa@gmail.com>
2021-03-26 08:07:31 +0300

    checking if we are in the morning

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import selenium.webdriver as webdriver
 import time
 import os
+import datetime as dt
+from datetime import datetime
+
+noon = dt.time(12, 0, 0)
+print(noon)
+time_now = datetime.now().time()
+print("morning = ", time_now < noon)
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(30)
@@ -20,6 +27,8 @@ driver.get("https://intranet.etechconsulting-mg.com/my/attendances")
 attendance_sign_in = driver.find_element_by_xpath("/html/body/div[1]/main/div/div[2]/div[3]/a[2]")
 attendance_sign_out = driver.find_element_by_xpath("/html/body/div[1]/main/div/div[2]/div[3]/a[1]")
 
+attendance = attendance_sign_in if time_now < noon else attendance_sign_out
+
 time.sleep(15)
 
-attendance_sign_out.click()
+attendance.click()
